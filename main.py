0d929f5fec2c5a059cc99c3cd16d323812d19d0c
Marek Buco <buco.marek@icloud.com>
2026-01-03 23:40:28 +0100

    implement multiple framebufers

--- main.py
+++ main.py
@@ -18,8 +18,7 @@ ds = DS1302(machine.Pin(0),machine.Pin(5),machine.Pin(4)) # pini pre hodiny rtc
 
 
 rtc = machine.RTC()# hodiny
-rtc_e = ds.date_time() # externe rtc
-rtc.datetime(ds.date_time()+ [0])
+rtc.datetime(ds.date_time()+[0])
 #rtc.datetime((rtc_e[0],rtc_e[1],rtc_e[2],0,rtc_e[4],rtc_e[5],rtc_e[6],0))
 
 
@@ -44,7 +43,7 @@ def GPS():
             return (gps.date_string('short'), gps.timestamp, gps.latitude_string(),
                   gps.longitude_string(), gps.altitude, '{:02.2f}'.format(gps.speed[2]))
             
-# init display         
+# init pre display         
 # pini pre display
 # pin_spi = 18
 # pin_mosi = 19
@@ -55,20 +54,38 @@ cs = machine.Pin(17, machine.Pin.OUT, value=0)
 fbuf = ST7920(spi, cs)
 
 
-fbuf_gps = framebuf.FrameBuffer(bytearray(128*64 // 2), 128, 64,framebuf.MONO_HLSB) # pre gps rýchlosť
+#fbuf_test =framebuf.FrameBuffer(bytearray(128*64 // 2), 20, 20,framebuf.MONO_HLSB)
+fbuf_sensor = framebuf.FrameBuffer(bytearray(312), 60, 39,framebuf.MONO_HLSB) #60*39 = 2 340 pocet pixelov / 8  = 292,5 pocet potrebných bajtov 
+fbuf_time = framebuf.FrameBuffer(bytearray(80), 64 ,10 ,framebuf.MONO_HLSB)
+fbuf_date = framebuf.FrameBuffer(bytearray(40), 30, 10,framebuf.MONO_HLSB)
+fbuf_gps = framebuf.FrameBuffer(bytearray(312), 124, 19,framebuf.MONO_HLSB) 
+
 
 def vip(data): #vykreslovanie-------
     fbuf.fill(0)
-    fbuf.rect(0,0,128,64,0xffff)
-    fbuf.text(data[0], 3, 31,0xffff)
-    fbuf.text(str(rtc.datetime()[2])+'.'+'0'+str(rtc.datetime()[1]),3,3,0xffff) 
-    fbuf.text(data[1], 3, 13, 0xffff)
-    fbuf.text(data[2], 3, 22, 0xffff)
-    fbuf.text(data[3], 60, 3, 0xffff)
-    fbuf.text('N',100,41,0xffff)
-    fbuf.text('E',100,51,0xffff)
-    fbuf.text( data[4], 3, 41,0xffff)
-    fbuf.text(data[5], 3, 51,0xffff)
+    fbuf_sensor.fill(0)
+    fbuf_time.fill(0)
+    fbuf_date.fill(0)
+    fbuf_gps.fill(0)
+    fbuf.rect(0,0,128,64,1)
+    
+    fbuf_sensor.text(data[0], 0, 18,1)
+    fbuf_sensor.text(data[1], 0, 0, 1)
+    fbuf_sensor.text(data[2], 0, 9, 1)
+    
+    fbuf_time.text(data[3], 0, 3, 1)
+    fbuf_date.text(data[4],0,3,1)
+    
+    fbuf_gps.text('N',100,0,1)
+    fbuf_gps.text('E',100,9,1)
+    fbuf_gps.text( data[5], 3, 0,1)
+    fbuf_gps.text(data[6], 3, 9,1)
+    
+    fbuf.blit(fbuf_sensor,3,15)
+    fbuf.blit(fbuf_time,58,2)
+    fbuf.blit(fbuf_date,3,2)
+    fbuf.blit(fbuf_gps,3,44)
+    
     fbuf.show()
 
 
@@ -92,20 +109,19 @@ def debug():
     print('hodinyS:',rtc.datetime()[6])
     print('display_nastavenie:',fbuf._spi)
     print('senzor:',sensor.get_measurement())
+    print(micropython.mem_info())
     
 
-    
-   
 def main(c):
     debug()
+    
     t, p , h = sensor.get_measurement()
+    Y, M, D, Day, Hr, m, S, ms = rtc.datetime()
     date, gcas, lat, lot, alt, spd = GPS()
     
-    vip( ('t{:+05.1f}C'.format(t), 'h{:02.0f}%'.format(h) , 'p{:04d}'.format(p // 100)       #nechitať
-          ,'{:02d}:{:02d}:{:02d}'.format(rtc.datetime()[4], rtc.datetime()[5], rtc.datetime()[6]) 
+    vip( ('t{:+05.1f}C'.format(t), 'h{:02.0f}%'.format(h) , 'p{:04d}'.format(p // 100)    #nechitať
+          ,'{:02d}:{:02d}:{:02d}'.format(Hr, M, S) , '{}.{}'.format(D,M)
           ,lat,lot))
    
-    
-    
 cas = machine.Timer() #casovac
 cas.init(period=1000, callback=main) #1000ms
